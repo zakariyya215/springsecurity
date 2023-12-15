93102aa088520544154e6415ef871c21e79783a1
zakariyya <1187203317@qq.com>
2023-12-15 14:40:09 +0800

    循环依赖问题

--- src/main/java/com/zakariyya/security01/Security01Application.java
+++ src/main/java/com/zakariyya/security01/Security01Application.java
@@ -1,9 +1,11 @@
 package com.zakariyya.security01;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zakariyya.security01.mapper")
 public class Security01Application {
 
 	public static void main(String[] args) {
